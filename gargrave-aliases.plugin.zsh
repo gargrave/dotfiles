154a7f69f83086e45abeba04fc571715c5d96d92
Gabe Hargrave <gkhargrave@gmail.com>
2019-06-01 10:47:58 -0700

    Git alias tweaks

--- gargrave-aliases.plugin.zsh
+++ gargrave-aliases.plugin.zsh
@@ -6,11 +6,12 @@ alias llg='ls -la | grep'
 
 # Git aliases
 alias gbr='git branch'
+alias gl="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --branches"
 alias glo='git log --oneline'
 alias gpl='git pull'
 alias gst='git status'
 alias gco='git checkout'
-alias gcob='git chcekout -b'
+alias gcob='git checkout -b'
 
 # Docker shortcuts
 alias dup='docker-compose up -d'
